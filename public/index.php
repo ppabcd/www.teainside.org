9dccc1d3132feb021c3b5cc31f22eb71420aeb2f
Ammar Faizi <ammarfaizi2@gmail.com>
2018-11-08 18:08:23 +0700

    Initial commit

---
+++ public/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tea Inside</title>
+	<style type="text/css">
+		* {
+			font-family: Arial;
+		}
+		a {
+			text-decoration: none;
+		}
+		a:hover {
+			text-decoration: underline;
+		}
+	</style>
+</head>
+<body>
+	<center>
+		<h1>This site is currently maintenance and should be back soon!</h1>
+		<h3>Contact: <a href="https://t.me/TeaInside">@TeaInside</a></h3>
+	</center>
+</body>
+</html>
